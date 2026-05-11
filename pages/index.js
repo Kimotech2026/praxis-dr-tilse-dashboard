a7e884f3e7ce7918ac1be5e2b2ae624956ba129e
Kimotech2026 <info@kimotech.de>
2026-05-11 21:43:02 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://opensheet.elk.sh/1AFGmKqR2typaxKARBprS81ArcBUqXg1RX8sXwNyO1oY/Tabellenblatt1")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div style={{ padding: 40 }}>
       <h1>Dr. Tilse Dashboard</h1>
-      <p>Dein Dashboard ist live 🚀</p>
+
+      <table border="1" cellPadding="10" style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>Datum</th>
+            <th>Uhrzeit</th>
+            <th>Name</th>
+            <th>Anliegen</th>
+            <th>Arzt</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((row, i) => (
+            <tr key={i}>
+              <td>{row.Datum}</td>
+              <td>{row.Uhrzeit}</td>
+              <td>{row.Name}</td>
+              <td>{row.Anliegen}</td>
+              <td>{row.Arzt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
